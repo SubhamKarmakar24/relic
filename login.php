01ca9a0c635d14da2bb0c417d4312877ab05556a
Subham Karmakar <rijukarmakar24@gmail.com>
2021-01-09 22:37:13 +0530

    New Login

--- login.php
+++ login.php
@@ -3,154 +3,117 @@
 require 'require.php';
 require 'connect.inc.php';
 
-if (isset($_POST['username']) && isset($_POST['password'])) {
+if (isset($_POST['username']) && isset($_POST['password']))
+{
 	
 	$username = $_POST['username'];
 	$password = $_POST['password'];
 	
-	if (!empty($username) && !empty($password)) {
-
+	if (!empty($username) && !empty($password))
+	{
 
 		$query0 = "SELECT password from user WHERE email = '$username'";
 		$result = $conn->query($query0);
-		$row = mysqli_fetch_row($result);
 		
-		if($result->num_rows == 1)
+		$state = false;
+		if($result->num_rows > 0)
     	{
-        	
-    	}
-		$pass = $row[0];
+			$state = true;
+			$row = mysqli_fetch_row($result);
+    	
+			$pass = $row[0];
 
+			if($state)
+			{
+				$status = password_verify( $password , $pass );
+					
+				if($status)
+				{
+					$query1 = "SELECT name, email from user WHERE email = '$username'";
+					$result = $conn->query($query1);
 
-		// $query0= "SELECT password FROM user WHERE email = ?";
-		// $stmt0 = $conn->prepare($query0);
-		// $stmt0->bind_param("s",$username);
-		$state=true;
-		// $pass ="";
-		// if($stmt0->execute()){
-		// while ($row0 = $stmt0->fetch()) {
+					$found = false;
+					if($result->num_rows > 0)
+					{
+						$row = mysqli_fetch_row($result);
+						$found = true;
+						$name = $row[0];
+						$ktjid = $row[1];
+					}
 				
-		// 		$pass = $row0[0];
-		// 		$state=true;
-
-		// 	}
-		// }
-		if($state){
-			
-			
-		// $status = password_verify ( $password , $pass );
-		$status = password_verify( $password , $row[0] );
-		echo $status;
-			
-		if($status){
-		$query = "SELECT  name, member_id FROM user WHERE email = ?";
-		$stmt = $conn->prepare($query);
-		$stmt->bind_param("s",$username);
-		
-
-		$found = false;
-		if ($stmt->execute()) {
-		
-
-			while ($row = $stmt->fetch()) {
 				
-				$name = $row[0];
-				$ktj_id = $row[1] ;
-			
-				
-				$found = true;
+					$query2 = "SELECT q_on, last_time, score, rank FROM relic WHERE email = '$username' AND ktj_id = '$ktjid'";
+					$result = $conn->query($query2);
 
+					$status= false;
+					
+					if($result->num_rows > 0)
+					{
+						$row = mysqli_fetch_row($result);
+						$status = true;
+						$q_on = $row[0];
+						$last_time = $row[1];
+						$score = $row[2];
+						$rank = $row[3];
+					}
+
+					if($status)
+					{
+					
+						$_SESSION['name'] = $name;
+						$_SESSION['email'] = $username;
+						$_SESSION['ktj_id'] = $ktjid;
+						$_SESSION['q_on'] = $q_on;
+						$_SESSION['last_time'] = $last_time;
+						$_SESSION['score'] = $score;
+						$_SESSION['rank'] = $rank;
+						echo "allwell";
+					}
+					else
+					{
+						$null = 'NULL';
+						$login_time= time();
+						$login_score= 1548527399-$login_time;
+						$zero='0';
+						$one= '1';
+
+						$query3 = "INSERT INTO relic (name, email, ktj_id, q_on, last_time, score, rank) VALUES ( '$name' , '$username' ,'$ktjid' ,'$one', '$login_time', '$login_score', $null )";
+						if($conn->query($query3) === true)
+						{
+							echo "New record created successfully";
+							$_SESSION['name'] = $name;
+							$_SESSION['email'] = $username;
+							$_SESSION['ktj_id'] = $ktjid;
+							$_SESSION['q_on'] = $one;
+							$_SESSION['last_time'] = $login_time;
+							$_SESSION['score'] = $login_score;
+							$_SESSION['rank'] = $zero;
+							echo "allwell";
+						}
+						else
+						{
+							echo "Error: " . $query3 . "<br>" . $conn->error;
+						}
+
+					}
+				}
+				else
+				{
+					echo 'Incorrect password';
+				}
 			}
 		}
-		
-			$query1 = "SELECT q_on, last_time, score, rank FROM relic WHERE email = ? AND ktj_id= ?";
-			$stmt1 = $conn->prepare($query1);
-		
-			$stmt1->bind_param(1,$username);
-			$stmt1->bind_param(2,$ktj_id);
-
-			$status= false;
-			if($stmt1->execute()){
-				while ($row = $stmt1->fetch()) {
-				
-				$q_on = $row[0];
-				$last_time = $row[1];
-				
-				$score = $row[2];
-				$rank = $row[3];
-			
-				
-				$status = true;
-
-			}
+		else
+		{
+			echo 'Email address not found, please register on ktj.in first';
 		}
-		if($status){
-			
-			$_SESSION['name'] = $name;
-			$_SESSION['email'] = $username;
-			$_SESSION['ktj_id'] = $ktj_id;
-			$_SESSION['q_on'] = $q_on;
-			$_SESSION['last_time'] = $last_time;
-		
-			$_SESSION['score'] = $score;
-			$_SESSION['rank'] = $rank;
-			echo "allwell";
-
-			
-
-
-		}else{
-			
-		$null = 'NULL';
-        $query2 = "INSERT INTO relic VALUES ( :id , :name , :email ,:ktj_id ,:q_on, :last_time, :score, :rank )";
-        $stmt2 = $conn->prepare($query2);
-        $zero='0';
-        $one= '1';
-        $login_time= time();
-        $login_score= 1548527399-$login_time;
-        $stmt2->bind_param(':id',$null);
-        $stmt2->bind_param(':name',$name);
-        $stmt2->bind_param(':email',$username);
-        $stmt2->bind_param(':ktj_id',$ktj_id);
-        $stmt2->bind_param(':q_on', $one);
-        $stmt2->bind_param(':last_time', $login_time);
-        $stmt2->bind_param(':score', $login_score);
-        $stmt2->bind_param(':rank', $null);
-
-
-			
-		
-        if (($stmt2->execute())) {
-         	//echo "Successful registration";
-			$_SESSION['name'] = $name;
-			$_SESSION['email'] = $username;
-			$_SESSION['ktj_id'] = $ktj_id;
-			$_SESSION['q_on'] = $one;
-			$_SESSION['last_time'] = $login_time;
-			$_SESSION['score'] = $login_score;
-			$_SESSION['rank'] = $zero;
-			
-			echo "allwell";
-
-        } else {
-          echo "Some error happened while registration";
-        
-        }
-
-
-
-
-		}
-	}else {
-			echo 'Incorrect password';
-		}
-	}else{
-		echo 'Email address not found, please register on ktj.in first';
 	}
-	} else {
+	else
+	{
 		echo 'Enter Some data';
 	}
-} else 
+}
+else 
 	header('Location: index.php');
 
 
