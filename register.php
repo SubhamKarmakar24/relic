2e2a70d3bfe0dc8d89de4cc564ddac1c28046327
Subham Karmakar <rijukarmakar24@gmail.com>
2021-01-10 13:00:25 +0530

    conn

--- register.php
+++ register.php
@@ -13,14 +13,6 @@ $pass = password_hash($password, PASSWORD_DEFAULT);
 
 // Database connection
 
-$conn = new mysqli('localhost','root','','kshitij');
-if($conn->connect_error)
-{
-    echo "$conn->connect_error";
-    die("Connection Failed : ". $conn->connect_error);
-}
-else
-{
 
     $sql = "SELECT email from user WHERE email = '$email'";
     $result = $conn->query($sql);
@@ -41,5 +33,4 @@ else
     
     $stmt->close();
     $conn->close();
-}
 ?>
